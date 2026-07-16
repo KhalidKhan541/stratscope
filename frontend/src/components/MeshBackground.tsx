47cffdba2059b1e651e91126fc70c21e6574ae84
KhalidKhan541 <khalidkhan46572@gmail.com>
2026-07-16 14:34:35 +0500

    feat(frontend): add MeshBackground.tsx

---
+++ frontend/src/components/MeshBackground.tsx
@@ -0,0 +1,10 @@
+export default function MeshBackground() {
+  return (
+    <div className="mesh-bg">
+      <div className="mesh-orb mesh-orb-1" />
+      <div className="mesh-orb mesh-orb-2" />
+      <div className="mesh-orb mesh-orb-3" />
+      <div className="mesh-orb mesh-orb-4" />
+    </div>
+  );
+}
